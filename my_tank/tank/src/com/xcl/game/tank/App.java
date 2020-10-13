b0fa39008f7a842d4fbfa3cb3cd0bceb506dc33f
HuaiShiLingGeGe <xcl_mail@163.com>
2020-10-13 14:23:06 +0800

    第一次提交

---
+++ my_tank/tank/src/com/xcl/game/tank/App.java
@@ -0,0 +1,16 @@
+package com.xcl.game.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class App {
+
+	public App() {
+	}
+
+	public static void main(String[] args) {
+		TankFrame tankFrame = new TankFrame();
+	}
+}
---
+++ my_tank/tank/src/com/xcl/game/tank/TankFrame.java
@@ -0,0 +1,28 @@
+package com.xcl.game.tank;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+	public TankFrame() {
+		setResizable(false);
+		setTitle("坦克大战");
+		setVisible(true);
+		setSize(800, 600);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.fillRoundRect(100, 100, 50, 50, 10, 10);
+	}
+
+}
